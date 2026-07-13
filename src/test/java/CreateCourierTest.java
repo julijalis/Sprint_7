ce48c68ce13ffcff60f5f3e3e5dd78adfa02940d
Yuliya Lisichkina <yulialisi4kina@yandex.ru>
2026-07-13 18:12:31 +0300

    fix: corrections after second code review

--- src/test/java/CreateCourierTest.java
+++ src/test/java/CreateCourierTest.java
@@ -18,15 +18,13 @@ public class CreateCourierTest extends BaseApiTest {
     @DisplayName("Create courier success")
     @Description("Testing that creating courier is successfully done")
     public void createCourierSuccess() {
-        CourierModel courier = new CourierModel(LOGIN, PASSWORD, FIRSTNAME);
-        CourierLoginModel loginData = new CourierLoginModel(LOGIN, PASSWORD);
+        courier = new CourierModel(LOGIN, PASSWORD, FIRSTNAME);
+        loginData = new CourierLoginModel(LOGIN, PASSWORD);
 
         createCourier(courier)
                 .then()
                 .statusCode(HTTP_CREATED)
                 .body("ok", equalTo(true));
-
-        courierId = loginAndGetCourierId(loginData);
     }
 
 
@@ -34,11 +32,10 @@ public class CreateCourierTest extends BaseApiTest {
     @DisplayName("Can't create same couriers")
     @Description("Testing that same couriers can't be created")
     public void cannotCreateSameCouriers() {
-        CourierModel courier = new CourierModel(LOGIN, PASSWORD, FIRSTNAME);
-        CourierLoginModel loginData = new CourierLoginModel(LOGIN, PASSWORD);
+        courier = new CourierModel(LOGIN, PASSWORD, FIRSTNAME);
+        loginData = new CourierLoginModel(LOGIN, PASSWORD);
 
         createCourier(courier);
-        courierId = loginAndGetCourierId(loginData);
 
         createCourier(courier)
                 .then()
@@ -50,14 +47,13 @@ public class CreateCourierTest extends BaseApiTest {
     @DisplayName("Can't create couriers with same login")
     @Description("Testing that couriers with same login can't be created")
     public void cannotCreateCouriersWithSameLogin() {
-        CourierModel courier1 = new CourierModel(LOGIN, PASSWORD, FIRSTNAME);
-        CourierLoginModel loginData = new CourierLoginModel(LOGIN, PASSWORD);
-        CourierModel courier2 = new CourierModel(LOGIN, "7490", "Мария");
+        courier = new CourierModel(LOGIN, PASSWORD, FIRSTNAME);
+        loginData = new CourierLoginModel(LOGIN, PASSWORD);
+        CourierModel courierWithDuplicatedLogin = new CourierModel(LOGIN, "7490", "Мария");
 
-        createCourier(courier1);
-        courierId = loginAndGetCourierId(loginData);
+        createCourier(courier);
 
-        createCourier(courier2)
+        createCourier(courierWithDuplicatedLogin)
                 .then()
                 .statusCode(HTTP_CONFLICT)
                 .body("message", equalTo("Этот логин уже используется"));
@@ -87,8 +83,12 @@ public class CreateCourierTest extends BaseApiTest {
 
     @After
     public void cleanUp () {
-        if (courierId != null) {
-            deleteCourier(courierId);
+        if (loginData != null) {
+            courierId = loginAndGetCourierId(loginData);
+
+            if (courierId != null) {
+                deleteCourier(courierId);
+            }
         }
     }
 }
